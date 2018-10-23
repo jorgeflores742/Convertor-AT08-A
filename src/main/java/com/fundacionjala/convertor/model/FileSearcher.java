4df972c0bc712a42b9bc710a3f7986b8ed801e5f
DennisMont <dmmv429@gmail.com>
2018-10-23 13:27:15 -0400

    Copyright added

--- src/main/java/com/fundacionjala/convertor/model/FileSearcher.java
+++ src/main/java/com/fundacionjala/convertor/model/FileSearcher.java
@@ -1,3 +1,17 @@
+/*
+ * @Controller.java Copyright (c) 2018 Fundacion Jala. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * Please contact Fundacion Jala, 2643 Av Melchor Perez de Olguin, Colquiri
+ * Sud, Cochabamba, Bolivia. www.fundacion-jala.org if you need additional
+ * information or have any questions.
+ */
 package com.fundacionjala.convertor.model;
 
 import java.io.File;
