65a9081a441afe3f0849edeb1b516b327d883596
wiltrouble <wiltrouble@gmail.com>
2018-10-30 13:33:05 -0400

    search creation time

--- src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
+++ src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,4 +19,14 @@ public class AdvancedSearchVideo {
         Map<String, Object> attribs = Files.readAttributes(path, attribList);
         return (String) attribs.get("lastModifiedTime");
     }
+
+    public String creationFileTime(String creationTimeFile) throws IOException {
+        Path  path   = Paths.get(creationTimeFile);
+        BasicFileAttributeView bfv = Files.getFileAttributeView(path,  BasicFileAttributeView.class);
+        BasicFileAttributes bfa  = bfv.readAttributes();
+
+        String creationT = String.valueOf(bfa.creationTime());
+        return creationT;
+    }
+
 }
