51d4e05fd0a26b6a19fc348a9685ad7fead261d0
wiltrouble <wiltrouble@gmail.com>
2018-10-30 13:20:36 -0400

    search by last modified time

--- src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
+++ src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
@@ -1,19 +1,19 @@
 package com.fundacionjala.convertor.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class AdvancedSearchVideo {
-
-    public static Map<Integer, String> readAttributes(Path path,
-                                                      String attributes,
-                                                      LinkOption... options){
-        Map<Integer, String> nombreMap = new HashMap<Integer, String>();
-
-        Map<Integer, String> nombreMap1 = nombreMap;
-        return nombreMap1;
+    public String searchByLastModifiedTime(String pathFile) throws IOException {
+        Path path = Paths.get(pathFile);
+        String attribList = "basic:size,lastModifiedTime";
+        Map<String, Object> attribs = Files.readAttributes(path, attribList);
+        return (String) attribs.get("lastModifiedTime");
     }
 }
