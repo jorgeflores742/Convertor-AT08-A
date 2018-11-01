43de6f0b649ac8d689af54cb9452b039a6d96485
wiltrouble <wiltrouble@gmail.com>
2018-11-01 16:06:10 -0400

    add ui in converted panel

--- src/main/java/com/fundacionjala/convertor/view/Converter.java
+++ src/main/java/com/fundacionjala/convertor/view/Converter.java
@@ -1,18 +1,39 @@
 package com.fundacionjala.convertor.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Converter extends JDialog{
     private JPanel player;
     private JLabel playerText;
+    private JButton btnConvert;
+    private JPanel converterBox1;
+    private JPanel converterBox2;
+    private JTextField pathFile;
 
     public Converter() {
         player = new JPanel();
         playerText = new JLabel();
-        playerText.setText("CONVERTER OPTIONS COMING SOON");
         player.add(playerText);
 
+        converterBox1 = new JPanel();
+        btnConvert = new JButton();
+        pathFile = new JTextField();
+
+
+        btnConvert.setText("CONVERT");
+        converterBox1.add(btnConvert);
+        player.add(converterBox1);
+
+        converterBox2 = new JPanel();
+        AdvancedSearchVideo asv = new AdvancedSearchVideo();
+        converterBox2.add(asv.getContentPane());
+        player.add(converterBox2);
         this.add(player);
         this.setResizable(false);
+
+
+
+
     }
 }
