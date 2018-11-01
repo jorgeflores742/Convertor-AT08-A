34a7f392baca0503f9dc0199a16dc94c38af329b
wiltrouble <wiltrouble@gmail.com>
2018-11-01 16:37:39 -0400

    add commom

--- src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
+++ src/main/java/com/fundacionjala/convertor/model/AdvancedSearchVideo.java
@@ -2,15 +2,26 @@ package com.fundacionjala.convertor.model;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.*;
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Module Controller SearchController.
+ *
+ * @author Wilson Lopez
+ * @version 1.0.
+ */
 
 public class AdvancedSearchVideo {
+
+    /**
+     *
+     * @param pathFile
+     * @return
+     * @throws IOException
+     */
     public String searchByLastModifiedTime(String pathFile) throws IOException {
         Path path = Paths.get(pathFile);
         String attribList = "basic:size,lastModifiedTime";
@@ -18,6 +29,12 @@ public class AdvancedSearchVideo {
         return (String) attribs.get("lastModifiedTime");
     }
 
+    /**
+     *
+     * @param creationTimeFile
+     * @return
+     * @throws IOException
+     */
     public String creationFileTime(String creationTimeFile) throws IOException {
         Path  path = Paths.get(creationTimeFile);
         BasicFileAttributeView bfv = Files.getFileAttributeView(path,  BasicFileAttributeView.class);
@@ -27,6 +44,12 @@ public class AdvancedSearchVideo {
         return creationT;
     }
 
+    /**
+     *
+     * @param pathFile
+     * @return
+     * @throws IOException
+     */
     public String fileOwnerAttributeView(String pathFile) throws IOException {
         Path path = Paths.get(pathFile);
         FileOwnerAttributeView foav = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
