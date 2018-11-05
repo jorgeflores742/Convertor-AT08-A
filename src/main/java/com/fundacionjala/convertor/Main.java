67a13e76d95af9c8c0adb576ab2bd3d10f38a28b
melvicaballero <you@example.com>
2018-11-05 12:50:13 -0400

    ffprobe.exe was adding

--- src/main/java/com/fundacionjala/convertor/Main.java
+++ src/main/java/com/fundacionjala/convertor/Main.java
@@ -3,30 +3,27 @@ package com.fundacionjala.convertor;
 import com.fundacionjala.convertor.controller.SearchController;
 import com.fundacionjala.convertor.model.FileSearcher;
 import com.fundacionjala.convertor.view.*;
-import com.sun.jna.NativeLibrary;
+
 
 /**
  * Main Class.
  */
 public class Main {
 
-
-    public static final String PATH_TO_VIDEO_LAN_VLC = "c:\\Program Files (x86)\\VideoLAN\\VLC\\";
-    public static final String PATH_TO_FFMPEG_BIN_FFPROBE = "c:\\otros\\ffmpeg\\bin\\ffprobe";
+    public static final String PATH_TO_FFMPEG_BIN_FFPROBE = "lib/filesff";
 
     /**
      * @param args arguments
      */
     public static void main(final String[] args) {
-        NativeLibrary.addSearchPath("libvlc", PATH_TO_VIDEO_LAN_VLC);
 
         SearchViewer searchViewer = new SearchViewer();
         AdvancedSearchVideo advancedSearchVideo = new AdvancedSearchVideo();
         AdvancedSearchAudio advancedSearchAudio = new AdvancedSearchAudio();
         ListFileView listFileView = new ListFileView();
         FileSearcher fileSearcher = new FileSearcher();
-        SearchController searchController = new SearchController(searchViewer, advancedSearchVideo, advancedSearchAudio,listFileView, fileSearcher);
-        ViewConverter v = new ViewConverter(searchViewer ,advancedSearchAudio, advancedSearchVideo,listFileView);
+        SearchController searchController = new SearchController(searchViewer, advancedSearchVideo, advancedSearchAudio, listFileView, fileSearcher);
+        ViewConverter v = new ViewConverter(searchViewer, advancedSearchAudio, advancedSearchVideo, listFileView);
         v.setVisible(true);
     }
 }
