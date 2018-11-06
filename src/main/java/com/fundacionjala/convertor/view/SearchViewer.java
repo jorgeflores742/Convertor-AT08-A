a2ce1970df5c53ab1172df3b0762800ec6b5fc58
DennisMont <dmmv429@gmail.com>
2018-11-06 12:32:44 -0400

    Search by audio improved

--- src/main/java/com/fundacionjala/convertor/view/SearchViewer.java
+++ src/main/java/com/fundacionjala/convertor/view/SearchViewer.java
@@ -200,6 +200,7 @@ public class SearchViewer extends JDialog implements ActionListener {
             int returnVal = chooser.showOpenDialog(this);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 txtPath.setText(chooser.getSelectedFile().getAbsolutePath());
+                pnlMain.updateUI();
             }
         }
     }
