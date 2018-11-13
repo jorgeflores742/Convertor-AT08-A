7b49ec5a8ded2e5af7171ba36e92e6af1b4a734c
wiltrouble <wiltrouble@gmail.com>
2018-11-13 03:18:47 -0400

    add more fields

--- src/main/java/com/fundacionjala/convertor/view/NewSearchViewer.java
+++ src/main/java/com/fundacionjala/convertor/view/NewSearchViewer.java
@@ -21,6 +21,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Module View Searchview.
@@ -46,8 +47,34 @@ public class NewSearchViewer extends JDialog implements ActionListener {
     private JComboBox cmbSize;
 
     private JButton btnSearch;
-
     private JButton btnClearList;
+
+    private JLabel lblVideoType;
+    private JComboBox cmbVideoType;
+    ArrayList<String> typeList = new ArrayList<>(1);
+
+    private JLabel lblFps;
+    private JComboBox cmbFps;
+    ArrayList<Float> fpsList = new ArrayList(1);
+
+
+    private JLabel lblAspectRatio;
+    private JComboBox cmbAspectRatio;
+    ArrayList<String> aspectRatioList = new ArrayList<>(1);
+
+    private JLabel lblResolution;
+    private JComboBox cmbResolution;
+    ArrayList<String> resolutionList = new ArrayList<>(1);
+
+    private JLabel lblVideo;
+    private JLabel lblAudio;
+
+    private JLabel lblAudioType;
+    private JComboBox cmbAudioType;
+
+    private JLabel lblVideoFps;
+    private JComboBox cmbVideoFps;
+
     /**
      * Pane for default search.
      */
@@ -64,6 +91,20 @@ public class NewSearchViewer extends JDialog implements ActionListener {
         cmbFileType = new JComboBox(FileTypeEnum.values());
         btnSearch = new JButton();
         btnClearList = new JButton();
+
+        lblVideo = new JLabel();
+        lblAudio = new JLabel();
+
+        lblVideoType = new JLabel();
+        cmbVideoType = new JComboBox();
+
+        lblAudioType = new JLabel();
+        cmbAudioType = new JComboBox();
+
+        lblVideoFps = new JLabel();
+        cmbVideoFps = new JComboBox();
+
+
         initializeControls();
         initializeFrame();
 
@@ -98,6 +139,40 @@ public class NewSearchViewer extends JDialog implements ActionListener {
         btnClearList.setIcon(new ImageIcon("img\\deleteBlack.png"));
         btnClearList.setBackground(Color.orange);
         btnClearList.setBorder(null);
+
+        lblVideo.setIcon(new ImageIcon("img\\video.png"));
+        lblAudio.setIcon(new ImageIcon("img\\music.png"));
+
+        lblVideoType.setText("Type:");
+        cmbVideoType.addItem("All");
+        cmbVideoType.addItem("avi");
+        cmbVideoType.addItem("mpg");
+        cmbVideoType.addItem("mp4");
+        cmbVideoType.addItem("flv");
+        cmbVideoType.addItem("wmv");
+        cmbVideoType.addItem("mkv");
+
+        lblVideoFps.setText("Frames:");
+        cmbVideoFps.addItem("All");
+        cmbVideoFps.addItem(24f);
+        cmbVideoFps.addItem(25f);
+        cmbVideoFps.addItem(27f);
+        cmbVideoFps.addItem(29.9f);
+        cmbVideoFps.addItem(30f);
+        cmbVideoFps.addItem(60f);
+        cmbVideoFps.addItem(120f);
+        cmbVideoFps.addItem(144f);
+        cmbVideoFps.addItem(240f);
+
+        lblAudioType.setText("Type:");
+        cmbAudioType.addItem("All");
+        cmbAudioType.addItem("mp3");
+        cmbAudioType.addItem("wav");
+        cmbAudioType.addItem("ogg");
+        cmbAudioType.addItem("m4a");
+        cmbAudioType.addItem("wma");
+        cmbAudioType.addItem("aac");
+        cmbAudioType.addItem("flac");
 
     }
 
@@ -138,7 +213,7 @@ public class NewSearchViewer extends JDialog implements ActionListener {
         gridPane.fill = GridBagConstraints.NONE;
         gridPane.anchor = GridBagConstraints.LINE_END;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.4;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(lblName, gridPane);
 
@@ -147,68 +222,121 @@ public class NewSearchViewer extends JDialog implements ActionListener {
         gridPane.fill = GridBagConstraints.HORIZONTAL;
         gridPane.anchor = GridBagConstraints.CENTER;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.6;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(txtName, gridPane);
 
         gridPane.gridx = 0;
-        gridPane.gridy = 5;
+        gridPane.gridy = 3;
         gridPane.fill = GridBagConstraints.NONE;
         gridPane.anchor = GridBagConstraints.LINE_END;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.4;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(lblFileType, gridPane);
 
         gridPane.gridx = 1;
-        gridPane.gridy = 5;
+        gridPane.gridy = 3;
         gridPane.fill = GridBagConstraints.HORIZONTAL;
         gridPane.anchor = GridBagConstraints.CENTER;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.6;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(cmbFileType, gridPane);
 
         gridPane.gridx = 0;
-        gridPane.gridy = 3;
+        gridPane.gridy = 4;
         gridPane.fill = GridBagConstraints.NONE;
         gridPane.anchor = GridBagConstraints.LINE_END;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.4;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(lblSize, gridPane);
 
         gridPane.gridx = 1;
-        gridPane.gridy = 3;
+        gridPane.gridy = 4;
         gridPane.fill = GridBagConstraints.HORIZONTAL;
         gridPane.anchor = GridBagConstraints.CENTER;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.6;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
         pnlMain.add(cmbSize, gridPane);
 
-        
+        gridPane.gridx = 1;
+        gridPane.gridy = 5;
+        gridPane.fill = GridBagConstraints.CENTER;
+        gridPane.anchor = GridBagConstraints.CENTER;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(lblVideo, gridPane);
+
+        gridPane.gridx = 4;
+        gridPane.gridy = 5;
+        gridPane.fill = GridBagConstraints.CENTER;
+        gridPane.anchor = GridBagConstraints.CENTER;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(lblAudio, gridPane);
+
+        gridPane.gridx = 0;
+        gridPane.gridy = 6;
+        gridPane.fill = GridBagConstraints.NONE;
+        gridPane.anchor = GridBagConstraints.LINE_END;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(lblVideoType, gridPane);
+
+        gridPane.gridx = 1;
+        gridPane.gridy = 6;
+        gridPane.fill = GridBagConstraints.HORIZONTAL;
+        gridPane.anchor = GridBagConstraints.CENTER;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(cmbVideoType, gridPane);
+
+        gridPane.gridx = 3;
+        gridPane.gridy = 6;
+        gridPane.fill = GridBagConstraints.NONE;
+        gridPane.anchor = GridBagConstraints.LINE_END;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(lblAudioType, gridPane);
+
+        gridPane.gridx = 4;
+        gridPane.gridy = 6;
+        gridPane.fill = GridBagConstraints.HORIZONTAL;
+        gridPane.anchor = GridBagConstraints.CENTER;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(cmbAudioType, gridPane);
 
 
-/*        gridPane.gridx = 0;
+        gridPane.gridx = 0;
+        gridPane.gridy = 7;
+        gridPane.fill = GridBagConstraints.NONE;
+        gridPane.anchor = GridBagConstraints.LINE_END;
+        gridPane.ipady = 0;
+        gridPane.weightx = 0.2;
+        gridPane.weighty = 0;
+        pnlMain.add(lblVideoFps, gridPane);
+
+        gridPane.gridx = 1;
         gridPane.gridy = 7;
         gridPane.fill = GridBagConstraints.HORIZONTAL;
         gridPane.anchor = GridBagConstraints.CENTER;
         gridPane.ipady = 0;
-        gridPane.weightx = 0.6;
+        gridPane.weightx = 0.2;
         gridPane.weighty = 0;
-        pnlMain.add(btnSearch, gridPane);*/
+        pnlMain.add(cmbVideoFps, gridPane);
+
 
         this.add(pnlMain);
-
-/*        gridPane.gridx = 0;
-        gridPane.gridy = 6;
-        gridPane.fill = GridBagConstraints.VERTICAL;
-        gridPane.anchor = GridBagConstraints.CENTER;
-        gridPane.ipady = 0;
-        gridPane.weightx = 0.6;
-        gridPane.weighty = 0;
-        pnlMain.add(btnClearList, gridPane);*/
         this.setResizable(false);
     }
 
