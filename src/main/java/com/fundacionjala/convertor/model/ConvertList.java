c4960fc5f878c1e1facbefef6e287bd7aea855d1
Jorge Flores <jorge.florez@ceis.cs.umss.edu.bo>
2018-11-19 02:56:55 -0400

    add copyright

--- src/main/java/com/fundacionjala/convertor/model/ConvertList.java
+++ src/main/java/com/fundacionjala/convertor/model/ConvertList.java
@@ -1,3 +1,18 @@
+/**
+ * @(#)ConvertList.java Copyright (c) 2018 Fundacion Jala. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * <p>
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * <p>
+ * Please contact Fundacion Jala, 2643 Av Melchor Perez de Olguin, Colquiri
+ * Sud, Cochabamba, Bolivia. www.fundacion-jala.org if you need additional
+ * information or have any questions.
+ */
+
 package com.fundacionjala.convertor.model;
 
 import java.io.BufferedWriter;
@@ -9,12 +24,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+/**
+ * Class ConvertList.
+ *
+ * @author Jorge Flores
+ * @version 1.0
+ */
+
 public class ConvertList {
 
     List<String> listTxt;
 
     public String[] convertLis() {
-        try {   
+        try {
             File file = new File("records.txt");
             // Si el archivo no existe, se crea!
             if (!file.exists()) {
